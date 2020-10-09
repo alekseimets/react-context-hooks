80d6fc735ee23c8d68847b7393e47e9bcd9f7b47
alekseimets <alekseimets@gmail.com>
2020-10-09 19:19:16 +0300

    Added Navbar in App

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <nav>
+                <h1>Context App</h1>
+                <uli>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </uli>
+            </nav>
         )
     }
 }
