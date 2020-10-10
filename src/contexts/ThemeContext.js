34630400543ee8ec6d9efa0e23034288dd90d123
alekseimets <alekseimets@gmail.com>
2020-10-10 22:25:57 +0300

    Added App themecontextproder

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -11,7 +11,7 @@ class ThemeContextProvider extends Component {
     render() { 
         return (
             <ThemeContext.Provider value={{...this.state}}>
-
+                
             </ThemeContext.Provider>
          );
     }
