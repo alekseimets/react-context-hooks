c54c1224f0292c35e9a32b9f3c31d1a68e22659f
alekseimets <alekseimets@gmail.com>
2020-10-24 22:41:14 +0300

    Deleting App.css

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import Navbar from './components/Navbar';
 import BookContextProvider from './contexts/BookContex';
 
 function App() {
   return (
     <div className="App">
     <BookContextProvider>
-      
+      <Navbar />
     </BookContextProvider>
     </div>
   );
